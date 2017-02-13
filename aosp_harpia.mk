06c50e48fd637c7d941ed587a97317adf452caaf
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-02-13 23:06:34 +0530

    AOSP bringup

--- aosp_harpia.mk
+++ aosp_harpia.mk
@@ -21,8 +21,8 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := harpia
-PRODUCT_NAME := lineage_harpia
 PRODUCT_MODEL := Moto G Play
+PRODUCT_NAME := aosp_harpia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := harpia
